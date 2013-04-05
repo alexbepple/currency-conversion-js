4d5fbb0fdb0180332024cec269a7ba06c0c20236
Alex Bepple <alex@bepple.de>
2013-04-05 02:50:36 +0200

    + load remote HTML on form submit

--- boilerplate/js/main.js
+++ boilerplate/js/main.js
@@ -1,3 +1,7 @@
-$.getJSON('http://whateverorigin.org/get?url=' + encodeURIComponent('http://www.bepple.de') + '&callback=?', function(data){
-    $('#content').html(data.contents);
+
+$('#currency-form').submit(function() {
+    $.getJSON('http://whateverorigin.org/get?url=' + encodeURIComponent('http://www.bepple.de') + '&callback=?', function(data){
+        $('#conversion-rate').html(data.status.http_code);
+    });
+    return false;
 });
