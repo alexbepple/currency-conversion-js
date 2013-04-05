6a145351a0be55fa8dbc74d3b32e29f5d944f90b
Alex Bepple <alex@bepple.de>
2013-04-05 04:45:21 +0200

    ~ extract method

--- boilerplate/js/main.js
+++ boilerplate/js/main.js
@@ -1,3 +1,25 @@
+var symbols;
+
+var updateConversionRate = function(from, to) {
+    if (!(from in symbols)) throw new Error('Invalid from currency: ' + from);
+    if (!(to in symbols)) throw new Error('Invalid to currency: ' + to);
+
+    var url = 'http://www.gocurrency.com/v2/dorate.php?inV=1&from=' + from + 
+        '&to=' + to + '&Calculate=Convert';
+    var anyOriginUrl = 'http://anyorigin.com/get?url=' + encodeURIComponent(url) + '&callback=?';
+    $.getJSON(anyOriginUrl, function(data){
+        var page = data.contents;
+        var start = page.lastIndexOf('<div id="converter_results"><ul><li>');
+        var substring = page.substring(start);
+        var startOfInterestingStuff = substring.indexOf('<b>') + 3;
+        var endOfInterestingStuff = substring.indexOf('</b>', startOfInterestingStuff);
+        var interestingStuff = substring.substring(startOfInterestingStuff, endOfInterestingStuff);
+        var parts = interestingStuff.split('=');
+        var value = parts[1].trim().split(' ')[0];
+        $('#conversion-rate').html(value);
+    });
+};
+
 $('#currency-form').submit(function() {
     var from = $('#from').val();
     var to = $('#to').val();
@@ -6,7 +28,7 @@ $('#currency-form').submit(function() {
     var anyOriginUrl = 'http://anyorigin.com/get?url=' + encodeURIComponent(url) + '&callback=?';
     $.getJSON(anyOriginUrl, function(data){
         var page = data.contents;
-        var symbols = {};
+        symbols = {};
         var currencyRegex = /href="\/currency\/[^>]+>(...)<\/a><\/td><td class="[^"]+">([A-Za-z ]+)/g;
         
         var match;
@@ -14,23 +36,7 @@ $('#currency-form').submit(function() {
             symbols[match[1]] = match[2];
         }
 
-        if (!(from in symbols)) throw new Error('Invalid from currency: ' + from);
-        if (!(to in symbols)) throw new Error('Invalid to currency: ' + to);
-
-        var url = 'http://www.gocurrency.com/v2/dorate.php?inV=1&from=' + from + 
-            '&to=' + to + '&Calculate=Convert';
-        var anyOriginUrl = 'http://anyorigin.com/get?url=' + encodeURIComponent(url) + '&callback=?';
-        $.getJSON(anyOriginUrl, function(data){
-            var page = data.contents;
-            var start = page.lastIndexOf('<div id="converter_results"><ul><li>');
-            var substring = page.substring(start);
-            var startOfInterestingStuff = substring.indexOf('<b>') + 3;
-            var endOfInterestingStuff = substring.indexOf('</b>', startOfInterestingStuff);
-            var interestingStuff = substring.substring(startOfInterestingStuff, endOfInterestingStuff);
-            var parts = interestingStuff.split('=');
-            var value = parts[1].trim().split(' ')[0];
-            $('#conversion-rate').html(value);
-        });
+        updateConversionRate(from, to);
     });
     
 
