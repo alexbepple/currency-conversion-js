52a9fdd74fc3fa0472423d0d9bf314e576bdfbc8
Alex Bepple <alex@bepple.de>
2013-04-10 19:15:27 +0200

    ~ stub even more cleanly

--- spec/CurrencyRateMonkeyPatchingSpec.js
+++ spec/CurrencyRateMonkeyPatchingSpec.js
@@ -1,24 +1,30 @@
 describe('Currency-rate retriever', function() {
     var original;
+    var returnHtml;
 
     beforeEach(function() {
         original = {getJSON: $.getJSON};
         loadFixtures('rate.html');
+
+        returnHtml = (function() {
+            var returnedHtml = [];
+            spyOn($, 'getJSON').andCallFake(function(_url, callback) { 
+                callback(stubResponse(returnedHtml.shift()));
+            });
+            var stubResponse = function(html) {
+                return {contents: html};
+            };
+
+            return function(html) {
+                returnedHtml.push(html);
+            };
+        })();
     });
     afterEach(function() {
         $.getJSON = original.getJSON;
     });
 
-    var stubResponse = function(html) {
-        return {contents: html};
-    };
-
     it('finds conversion rate', function() {
-        var returnedHtml = [];
-        spyOn($, 'getJSON').andCallFake(function(_url, callback) { 
-            callback(stubResponse(returnedHtml.shift()));
-        });
-
         var fakeCurrencyHtml = 
             'href="/currency/foo>USD</a></td><td class="baz">foo</td>' +
             'href="/currency/foo>EUR</a></td><td class="baz">bar</td>';
@@ -26,8 +32,8 @@ describe('Currency-rate retriever', function() {
         var fakeRatesHtml = 
             '<div id="converter_results"><ul><li><b>1 x = 2 y</b>';
 
-        returnedHtml.push(fakeCurrencyHtml);
-        returnedHtml.push(fakeRatesHtml);
+        returnHtml(fakeCurrencyHtml);
+        returnHtml(fakeRatesHtml);
 
         reactOnSubmitForm();
 
